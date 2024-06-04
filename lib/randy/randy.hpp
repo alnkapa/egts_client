d32c799c55ac7e2ba68cfb9604b02317ff54054b
Alexander A. Kapralov <alnkapa@gmail.com>
2024-06-04 15:12:35 +0300

    randy

--- lib/randy/randy.hpp
+++ lib/randy/randy.hpp
@@ -2,61 +2,27 @@
 #ifndef RANDY_HPP
 #define RANDY_HPP
 
-#include <cstdint>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <iomanip>
-#include <iostream>
+#include <algorithm>
+#include <array>
 #include <random> // random_device,default_random_engine
 
 namespace randy
 {
-    class Bytes
+    template <typename T, std::size_t N>
+    class Array : public std::array<T, N>
     {
     public:
-        using bytes_t = std::vector<std::uint8_t>;
-        Bytes() = default;
-        Bytes(const bytes_t &data) : m_data(data) {}
-        Bytes(bytes_t &&data) : m_data(std::move(data)) {}
-
-        const bytes_t &data() const { return m_data; }
-        bytes_t &data() { return m_data; }
-
-        uint8_t &operator[](std::size_t index) { return m_data[index]; }
-        const uint8_t &operator[](std::size_t index) const { return m_data[index]; }
-
-        std::string to_hex_string()
+        Array() : std::array<T, N>()
         {
-            std::stringstream stream;
-            stream.flags(std::ios::hex | std::ios::uppercase);
-            for (auto &&b : m_data)
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<T> distribution(0, UINT8_MAX);
+            for (int i = 0; i < N; i++)
             {
-                if (b)
-                {
-                    stream << std::setw(2) << std::setfill('0') << static_cast<int>(b);
-                }
-            };
-            return stream.str();
-        }
-
-    private:
-        bytes_t m_data;
+                (*this)[i] = distribution(gen);
+            }
+        };
     };
-    template <std::size_t N>
-    constexpr Bytes random()
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<std::uint8_t> distribution(0, UINT8_MAX);
-        Bytes::bytes_t ret{};
-        ret.reserve(N);
-        for (int i = 0; i < N; i++)
-        {
-            ret[i] = distribution(gen);
-        }
-        return ret;
-    }
 }
 
 #endif /* RANDY_HPP */
--- main.cpp
+++ main.cpp
@@ -1,41 +1,51 @@
-// #include <iostream>
-// #include "lib/egts/transport/transport.hpp"
-// #include "lib/randy/randy.hpp"
-// #include "lib/endian/endian.hpp"
-
-// int main(int argv, const char **args)
-// {
-//     egts::transport::Packet tr{};
-//     std::cin >> tr;
-//     constexpr size_t max = 11;
-//     auto bb = randy::random<max>();
-//     char b[]{1, 2, 3, 4, 5, 6};
-//     std::cin >> tr;
-//     std::cout << tr << "\n";
-//     return EXIT_SUCCESS;
-// }
-
-#include <cstdlib>
 #include <iostream>
+#include "lib/egts/transport/transport.hpp"
+#include "lib/randy/randy.hpp"
+#include "lib/endian/endian.hpp"
 
-#include <boost/context/fiber.hpp>
+int main(int argv, const char **args)
+{
+    auto bb = randy::Array<std::uint8_t,2>{};
+    for (auto &v : bb)
+    {
+        std::cout << std::hex << static_cast<int>(v);
+    }
+    // std::cout << "\n ";
+    // std::fill(bb.begin(), bb.end(), 3);
+    // for (auto &v : bb)
+    // {
+    //     std::cout << static_cast<int>(v);
+    // }
+    std::cout << "\n";
 
-namespace ctx = boost::context;
+    egts::transport::Packet tr{};
 
-ctx::fiber bar( ctx::fiber && f) {
-    do {
-        std::cout << "bar\n";
-        f = std::move( f).resume();
-    } while ( f);
-    return std::move( f);
-}
+    // вариант для чисел с плавающей точкой
 
-int main() {
-    ctx::fiber f{ bar };
-    do {
-        std::cout << "foo\n";
-        f = std::move( f).resume();
-    } while ( f);
-    std::cout << "main: done" << std::endl;
     return EXIT_SUCCESS;
 }
+
+// #include <cstdlib>
+// #include <iostream>
+
+// #include <boost/context/fiber.hpp>
+
+// namespace ctx = boost::context;
+
+// ctx::fiber bar( ctx::fiber && f) {
+//     do {
+//         std::cout << "bar\n";
+//         f = std::move( f).resume();
+//     } while ( f);
+//     return std::move( f);
+// }
+
+// int main() {
+//     ctx::fiber f{ bar };
+//     do {
+//         std::cout << "foo\n";
+//         f = std::move( f).resume();
+//     } while ( f);
+//     std::cout << "main: done" << std::endl;
+//     return EXIT_SUCCESS;
+// }
