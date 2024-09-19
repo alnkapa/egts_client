3f9d3292a3d264c967f47003a3c2856683814b7c
Alexander A. Kapralov <alnkapa@gmail.com>
2024-09-19 16:08:36 +0300

    pub_sub

---
+++ lib/pub_sub/pub_sub.h
@@ -0,0 +1,103 @@
+#ifndef PUB_SUB_H
+#define PUB_SUB_H
+#include <algorithm>
+#include <forward_list>
+#include <functional>
+#include <memory>
+#include <mutex>
+
+namespace pubsub
+{
+
+template <typename... T>
+class Publisher
+{
+  public:
+    struct Subscriber
+    {
+        virtual void
+        callback(T...) = 0;
+        virtual ~Subscriber() = default;
+    };
+
+    class Function : public Subscriber
+    {
+      private:
+        std::function<void(T...)> m_fu;
+
+      public:
+        Function(std::function<void(T...)> fu)
+            : m_fu(fu) {};
+        void
+        callback(T... args) override
+        {
+            m_fu(std::forward<T>(args)...);
+        };
+    };
+
+  private:
+    std::forward_list<std::weak_ptr<Subscriber>> subscribers;
+    std::mutex m;
+
+  public:
+    virtual ~Publisher() = default;
+
+    virtual void
+    subscribe(std::weak_ptr<Subscriber> in)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        subscribers.emplace_front(in);
+    }
+
+    virtual void
+    unsubscribe(std::weak_ptr<Subscriber> in)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        auto pred = subscribers.before_begin();
+        auto current = subscribers.begin();
+        auto in_locked = in.lock();
+        while (current != subscribers.end())
+        {
+            if (auto v = current->lock())
+            {
+                if (v == in_locked)
+                {
+                    current = subscribers.erase_after(pred);
+                }
+                else
+                {
+                    pred = current;
+                    ++current;
+                }
+            }
+            else
+            {
+                current = subscribers.erase_after(pred);
+            }
+        }
+    }
+
+    virtual void
+    notify(T... args)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        auto pred = subscribers.before_begin();
+        auto current = subscribers.begin();
+        while (current != subscribers.end())
+        {
+            if (auto l = current->lock())
+            {
+                (l->callback)(std::forward<T>(args)...);
+                pred = current;
+                ++current;
+            }
+            else
+            {
+                current = subscribers.erase_after(pred);
+            }
+        }
+    }
+};
+
+} // namespace pubsub
+#endif
---
+++ lib/pub_sub/tests/tests.cpp
@@ -0,0 +1,89 @@
+#include "../pub_sub.h"
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+
+class MySubscriber : public pubsub::Publisher<int>::Subscriber
+{
+  public:
+    MySubscriber(int id)
+        : id(id), receivedCount(0) {}
+
+    void
+    callback(int value) override
+    {
+        std::cout << "Subscriber " << id << " received: " << value << std::endl;
+        receivedCount++;
+    }
+
+    int
+    getReceivedCount() const
+    {
+        return receivedCount;
+    }
+
+  private:
+    int id;
+    int receivedCount;
+};
+
+void
+publisherThread(pubsub::Publisher<int> &publisher)
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        publisher.notify(i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void
+subscriberThread(pubsub::Publisher<int> &publisher, int id)
+{
+    auto subscriber = std::make_shared<MySubscriber>(id);
+    publisher.subscribe(subscriber);
+
+    // Подписчик будет получать уведомления в течение 5 секунд
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    // Отписываемся
+    publisher.unsubscribe(subscriber);
+}
+
+void
+concurrentTest(pubsub::Publisher<int> &publisher)
+{
+    // Запускаем поток для публикации
+    std::thread pubThread(publisherThread, std::ref(publisher));
+
+    // Запускаем несколько потоков для подписки
+    const int numSubscribers = 5;
+    std::vector<std::thread> subThreads;
+    for (int i = 0; i < numSubscribers; ++i)
+    {
+        subThreads.emplace_back(subscriberThread, std::ref(publisher), i + 1);
+    }
+
+    // Ждем завершения всех потоков
+    pubThread.join();
+    for (auto &t : subThreads)
+    {
+        t.join();
+    }
+}
+
+int
+main()
+{
+    pubsub::Publisher<int> publisher;
+
+    // Запускаем тест
+    concurrentTest(publisher);
+
+    std::cout << "All threads have finished." << std::endl;
+
+    return 0;
+}
