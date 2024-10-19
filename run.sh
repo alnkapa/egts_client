c6ed0af5046b62f7389e8a37096096d80cb3a9f7
Alexander A. Kapralov <alnkapa@gmail.com>
2024-10-19 09:36:34 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 mkdir -p build
 cd build
-# rm -rf *
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+rm -rf *
+cmake .. -DCMAKE_BUILD_TYPE=Release
+# cmake --build .
 cd ..
 # 863921034878280
-# 917097916630013
-# ./build/egts_client -e 917097916630013 -a www.my.uaz.ru:1111  -n nmea.txt
 ./build/egts_client -e 863921034878280 -a test.shatl-t.ru:36329  -n nmea.txt
